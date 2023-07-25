63ecaf5221cf2695304fa9a15ccf58d8f2b1bde4
AlexDolz <aleksejdolznikov3@gmail.com>
2023-07-25 13:30:57 +0200

    updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// *********** Converting and checking numbers ************************
 console.log(23 === 23.0);
 
 // Base 10 - 0 to 9
@@ -33,3 +34,104 @@ console.log(Number.isFinite(23 / 0)); // false
 console.log(Number.isInteger(23)); // true
 console.log(Number.isInteger(23.0)); // true
 console.log(Number.isInteger(23 / 0)); // false
+
+// ****************** Math and rounding ********************************
+
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+
+console.log(Math.max(5, 18, 12, 11, 2)); // 18
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInte = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInte(10, 20)); // random numbers from 10 to 20
+
+// Rounding integers
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor(23.9)); // 23
+console.log(Math.floor('23.9')); // 23
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // 3 string always when toFixed
+console.log((2.7).toFixed(3)); // 2.35
+console.log(+(2.345).toFixed(2)); // 2.35 number
+
+// ********************** Remainder operator ***************************
+
+console.log(5 % 2); // 1   2*2 = 4   5 - 4 = 1
+
+console.log(8 % 3); // 2
+
+console.log(6 % 2); // 0
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8)); // true  even
+console.log(isEven(23)); // false odd
+console.log(isEven(514)); // true even
+
+// ********************** Numeric separators ***************************
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+console.log(transferFee1);
+console.log(transferFee2);
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230_000')); // NaN
+console.log(parseInt('230_00')); // 230
+
+// ************************* BigInt **************************************
+
+console.log(2 ** 53 - 1); // 9007199254740991 the biggest number JS can safely represent
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+
+console.log(48543604580768345974097n);
+console.log(BigInt(48543604580768345974097));
+
+// Operations
+console.log(10000n + 10000n);
+console.log(329045349067450870458082835060387n * 100000000n);
+
+const huge = 21592345603496089345760n;
+const num = 23;
+// console.log(huge + num); // error, cant combine
+console.log(huge + BigInt(num));
+
+console.log(20n > 15); // true
+console.log(20n === 20); // false
+console.log(typeof 20n); // bigint
+console.log(20n == '20'); // true
+
+console.log(huge + ' is REALLY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);
